8fd553b1f3ec0a187b10b9f620b5ad7bb537d50c
kervyRivas <a20112128@pucp.pe>
2021-08-30 14:08:10 -0500

    adding buttons to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import Slider from '@material-ui/core/Slider';
+
+import { IconButton } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 import './Footer.css';
 
-const Footer = props => {
+const Footer = (props) => {
+  const sizeIconContext = useMemo(() => ({size:"6em"}), [])
+
 	return (
 		<div className="footer">	
-			This is the footer
+			<div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer__center">
+        <div className="footer__buttons">
+          <ShuffleIcon fontSize="small"/>
+          <SkipPreviousIcon/>
+          <PlayCircleFilledIcon className="footer_play-button"/>
+          <SkipNextIcon/>
+          <RepeatIcon fontSize="small"/>
+        </div>
+        <div className="footer__time-info">
+          <span>0:30</span>
+          <div className="footer__progress-bar">&nbsp;</div>
+          <span>5:39</span>
+        </div>
+      </div>
+      <div className="footer__rigth">
+        <PlaylistPlayIcon/>
+        <VolumeDownIcon/>
+        <Slider/>
+      </div>
 		</div>
 	);
 };
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import SidebarOption from './../SidebarOption';
-import HomeIcon from '@material-ui/icons/Home';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import SearchIcon from '@material-ui/icons/Search';
-import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import LibraryMusicOutlinedIcon from '@material-ui/icons/LibraryMusicOutlined';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -15,10 +17,12 @@ const Sidebar = ({
   return (
     <div className="sidebar">
       <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"></img>
-      <SidebarOption title={"Inicio"} Icon={HomeIcon}></SidebarOption>
+      <SidebarOption title={"Inicio"} Icon={HomeOutlinedIcon}></SidebarOption>
       <SidebarOption title={"Buscar"} Icon={SearchIcon}></SidebarOption>
-      <SidebarOption title={"Tu Biblioteca"} Icon={LibraryMusicIcon}></SidebarOption>
-      <br />
+      <SidebarOption title={"Tu Biblioteca"} Icon={LibraryMusicOutlinedIcon}></SidebarOption>
+      <div className="br"></div>
+      <SidebarOption title={"Crear playlist"} Icon={AddBoxIcon}></SidebarOption>
+      <SidebarOption title={"Tus me gusta"} Icon={FavoriteIcon}></SidebarOption>
       <hr />
       {playlists?.items?.map((item, index) => <SidebarOption title={item.name} key={index} /> )}
     </div>
