98efc1fdb61124c9af21a72cc9e526822738e5a3
kervyRivas <a20112128@pucp.pe>
2021-08-31 22:24:36 -0500

    adding header

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './../Header';
 import PropTypes from 'prop-types';
 
 import './Body.css';
@@ -6,6 +7,7 @@ import './Body.css';
 const Body = props => {
 	return (
 		<div className="body">
+      <Header/>
 			This is the body
 		</div>
 	);
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header(props) {
+  return (
+    <div className="header">
+      <div className="header__left">
+        left
+      </div>
+      <div className="header__rigth">
+        rigth
+      </div>
+    </div>
+  )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header
+
